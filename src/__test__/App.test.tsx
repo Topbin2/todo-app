7d86d72b6b260ed175ecc6dffce188d081e6c77f
MoSangBin <mosangbin94@gmail.com>
2022-08-08 15:27:05 +0900

    test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -5,5 +5,5 @@ import { render, screen } from "./test-utils";
 test("Home 화면이 렌더링 된다.", () => {
   render(<App />);
 
-  expect(screen.getByRole("heading", { name: /home/i })).toBeInTheDocument();
+  expect(screen.getByRole("heading", { name: /home11/i })).toBeInTheDocument();
 });
